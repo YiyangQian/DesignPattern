ceb36a459b5f33c2b6476fe66b83846b25de57ca
YY Qian <yy.qian@autodesk.com>
2020-05-02 19:48:13 -0700

    lazy loading solution

---
+++ src/com.yiyang/singleton/Mgr06.java
@@ -0,0 +1,37 @@
+package com.yiyang.singleton;
+
+/**
+ * lazy loading
+ * multi-thread safe
+ * efficient
+ */
+public class Mgr06 {
+    private static Mgr06 INSTANCE;
+
+    private Mgr06() {};
+
+    public static Mgr06 getInstance() {
+        if (INSTANCE == null) {
+            synchronized (Mgr06.class) {
+                if (INSTANCE == null) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    INSTANCE = new Mgr06();
+                }
+            }
+        }
+        return INSTANCE;
+    };
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(() ->
+                    System.out.println(Mgr06.getInstance().hashCode())
+            ).start();
+        }
+    }
+
+}
